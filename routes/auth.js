de9ce966069a57f7b4fd906bde692abba7bfcc13
Dhanamoorthy <dhanasekar9540@gmail.com>
2024-06-27 18:33:53 +0530

    error fixing

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ import express from 'express'
 import {User} from '../models/User.js'
 import bcrypt from 'bcrypt'
 import {Lead} from '../models/Lead.js'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 
